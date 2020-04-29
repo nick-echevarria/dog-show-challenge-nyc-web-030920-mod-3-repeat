ca619bb6cd05019d8972c1a23084ba7bb28db1bc
nick-echevarria <nick.echev@gmail.com>
2020-04-29 11:42:12 -0400

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", initialize)
 const dogUrl = "http://localhost:3000/dogs"
 const dogTable = document.querySelector("#table-body")
+const headers = { 
+    "Content-Type" : "application/json",
+    "Accept" : "application/json"
+}
 
 function initialize() { 
     fetchDogs()
@@ -32,10 +36,46 @@ function renderDogs(dogs) {
 
 function buttonSetup() { 
     const dogTable = document.querySelector("#table-body")
+    const editForm = document.querySelector("#dog-form")
+    // const submitButton = document.querySelectorAll("input")[3]
+
     dogTable.addEventListener("click", (e) => { 
         if (e.target.className === "edit-btn") { 
-            console.log(e.target.dataset.id)
-        }
+            populateEditForm(e.target.dataset.id)
+        } 
     })
+
+    editForm.addEventListener("submit", (e) => { 
+        event.preventDefault()
+        fetch(`${dogUrl}/${dogId}`, { 
+            method: "PATCH", 
+            headers, 
+            body: JSON.stringify({
+                name: e.target.name.value,
+                breed: e.target.breed.value,
+                sex: e.target.sex.value,
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        
+    })
+}
+
+function populateEditForm(dogId) { 
+
+    fetch(`${dogUrl}/${dogId}`)
+        .then(resp => resp.json())
+        .then(dog => {  
+            
+            let nameInput = document.querySelectorAll("input")[0]
+            let breedInput = document.querySelectorAll("input")[1]
+            let sexInput = document.querySelectorAll("input")[2]
+
+            nameInput.value = dog.name
+            breedInput.value = dog.breed
+            sexInput.value = dog.sex
+
+        })        
 }
 
